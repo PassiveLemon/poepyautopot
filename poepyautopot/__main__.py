fb71e854e22eb4275cb3170d3a4d4e984310a811
PassiveLemon <lemonl3mn@protonmail.com>
2023-07-25 18:36:57 -0400

    refactor: variable touchups

--- poepyautopot/__main__.py
+++ poepyautopot/__main__.py
@@ -7,6 +7,9 @@ import pyautogui as pag
 #import keyboard as kb
 from evdev import UInput, ecodes as e
 
+
+# Init
+
 def config_init():
   yaml_config = yaml.safe_load(open("./config.yaml"))
 
@@ -17,17 +20,22 @@ def config_init():
   screen_offset_x = screen_offset["x"]
   screen_offset_y = screen_offset["y"]
 
-  global key_press_shortest, key_press_longest, key_press_range, key_press_std_dev, key_press_target
+  global key_press_shortest, key_press_longest, key_press_range, key_press_std_dev, key_press_target, key_press_test_enable, key_press_test_delay, key_press_test_count
   key_press = yaml_config["key_press"]
-
   key_press_shortest = key_press["shortest"]
   key_press_longest = key_press["longest"]
   key_press_range = key_press["range"]
   key_press_std_dev = key_press["std_dev"]
   key_press_target = key_press["target"]
+  key_press_test = key_press["test"]
+  key_press_test_enable = key_press_test["enable"]
+  key_press_test_delay = key_press_test["delay"]
+  key_press_test_count = key_press_test["count"]
 
-  global image_save_location
-  image_save_location = yaml_config["image_save_location"]
+  global image_test_enable, image_test_save_location
+  image_test = yaml_config["image_test"]
+  image_test_enable = image_test["enable"]
+  image_test_save_location = image_test["save_location"]
 
 def flask_body_init():
   # Template for flasks
@@ -43,13 +51,31 @@ def flask_body_init():
     def __str__(self):
       return f"Object(use={self.use}, max={self.max}, cur={self.cur}, img={self.img})"
 
-  # Defining default values
+  # Defining default values. Will be moved to a config file
   global flask1, flask2, flask3, flask4, flask5
   flask1 = flask_body(enable=True, use=5, max=15, cur=max, img=None)
   flask2 = flask_body(enable=True, use=5, max=15, cur=max, img=None)
   flask3 = flask_body(enable=False, use=5, max=15, cur=max, img=None)
   flask4 = flask_body(enable=False, use=5, max=15, cur=max, img=None)
   flask5 = flask_body(enable=False, use=5, max=15, cur=max, img=None)
+
+def life_init():
+  global life_panel
+  life_panel = pag.screenshot(region=(100 + screen_offset_x, 875 + screen_offset_y, 2, 200))
+
+def mana_init():
+  global mana_panel
+  mana_panel = pag.screenshot(region=(1800 + screen_offset_x, 875 + screen_offset_y, 2, 200))
+
+def key_press_init():
+  global key_press_shortest_low, key_press_shortest_high, key_press_longest_low, key_press_longest_high
+  key_press_shortest_low = key_press_shortest - key_press_range
+  key_press_shortest_high = key_press_shortest + key_press_range
+  key_press_longest_low = key_press_longest - key_press_range
+  key_press_longest_high = key_press_longest + key_press_range
+
+
+# Utility
 
 def flask_capture():
   # Image with panel of flasks
@@ -62,35 +88,6 @@ def flask_capture():
   flask3.img = flasks_panel.crop((93, 0, 129, 80))
   flask4.img = flasks_panel.crop((139, 0, 175, 80))
   flask5.img = flasks_panel.crop((185, 0, 221, 80))
-
-def life_init():
-  global life_panel
-  life_panel = pag.screenshot(region=(100 + screen_offset_x, 875 + screen_offset_y, 2, 200))
-
-def mana_init():
-  global mana_panel
-  mana_panel = pag.screenshot(region=(1800 + screen_offset_x, 875 + screen_offset_y, 2, 200))
-
-def image_init_test():
-  flask_capture()
-  flasks_panel.save(image_save_location + "flasks_panel.png")
-
-  flask1.img.save(image_save_location + "flask1.png")
-  flask2.img.save(image_save_location + "flask2.png")
-  flask3.img.save(image_save_location + "flask3.png")
-  flask4.img.save(image_save_location + "flask4.png")
-  flask5.img.save(image_save_location + "flask5.png")
-
-  life_panel.save(image_save_location + "life.png")
-
-  mana_panel.save(image_save_location + "mana_panel.png")
-
-def key_press_init():
-  global key_press_shortest_low, key_press_shortest_high, key_press_longest_low, key_press_longest_high
-  key_press_shortest_low = key_press_shortest - key_press_range
-  key_press_shortest_high = key_press_shortest + key_press_range
-  key_press_longest_low = key_press_longest - key_press_range
-  key_press_longest_high = key_press_longest + key_press_range
 
 def key_press(num):
   # Bell curve to more closely group presses
@@ -105,11 +102,11 @@ def key_press(num):
   ui.write(e.EV_KEY, num, 1)
   ui.syn()
 
-  # Slightly variate keypress length range
+  # Slightly variate key press length range
   random_range_low = random.randint(key_press_shortest_low, key_press_shortest_high)
   random_range_high = random.randint(key_press_longest_low, key_press_longest_high)
 
-  # Variate keypress length with random lows and highs, q3, and std dev.
+  # Variate key press length with random lows and highs, std dev, and target.
   random_key_press_sleep = bell_curve(random_range_low, random_range_high, key_press_std_dev, key_press_target)
   time.sleep(random_key_press_sleep / 1000.0)
 
@@ -118,20 +115,18 @@ def key_press(num):
   ui.syn()
   ui.close()
 
-def key_press_test(slp, i):
-  # Give time to move cursor
-  time.sleep(slp)
+  # Print pressed key and its length
   key_map = {
-    1: e.KEY_1,
-    2: e.KEY_2,
-    3: e.KEY_3,
-    4: e.KEY_4,
-    5: e.KEY_5,
+    e.KEY_1: 1,
+    e.KEY_2: 2,
+    e.KEY_3: 3,
+    e.KEY_4: 4,
+    e.KEY_5: 5,
   }
-  while i > 0:
-    random_key = random.randint(1, 5)
-    key_press(key_map[random_key])
-    i -= 1
+  print(f"{key_map[num]} ({random_key_press_sleep} ms)")
+
+
+# Main
 
 def main():
   config_init()
@@ -142,9 +137,47 @@ def main():
 
 main()
 
+
 # Tests
-image_init_test()
-key_press_test(3, 20)
+
+def image_test():
+  flask_capture()
+  flasks_panel.save(image_test_save_location + "flasks_panel.png")
+
+  flask1.img.save(image_test_save_location + "flask1.png")
+  flask2.img.save(image_test_save_location + "flask2.png")
+  flask3.img.save(image_test_save_location + "flask3.png")
+  flask4.img.save(image_test_save_location + "flask4.png")
+  flask5.img.save(image_test_save_location + "flask5.png")
+
+  life_panel.save(image_test_save_location + "life.png")
+
+  mana_panel.save(image_test_save_location + "mana_panel.png")
+
+def key_press_test():
+  time.sleep(key_press_test_delay)
+  key_map = {
+    1: e.KEY_1,
+    2: e.KEY_2,
+    3: e.KEY_3,
+    4: e.KEY_4,
+    5: e.KEY_5,
+  }
+  i = key_press_test_count
+  while i > 0:
+    random_key = random.randint(1, 5)
+    key_press(key_map[random_key])
+    i -= 1
+
+def test():
+  if image_test_enable is True:
+    image_test()
+
+  if key_press_test_enable is True:
+    key_press_test()
+
+test()
+
 
 #runnerstate = False
 
