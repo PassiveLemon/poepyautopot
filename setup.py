372a353b9f66a13f394ae6dfc8c52e4f40deb2a9
PassiveLemon <lemonl3mn@protonmail.com>
2023-07-30 16:24:29 -0400

    commas...

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
   name="poepyautopot",
   version=0.1,
-  author="PassiveLemon"
-  author_email="jeremyseber@gmail.com"
-  description="A Python based Autopot script for Path of Exile"
-  packages=find_packages()
+  author="PassiveLemon",
+  author_email="jeremyseber@gmail.com",
+  description="A Python based Autopot script for Path of Exile",
+  packages=find_packages(),
   url="https://github.com/PassiveLemon/poepyautopot"
 )
