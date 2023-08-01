96495ed0073ca66f5ab09fd9e95a893d6b49c71c
PassiveLemon <lemonl3mn@protonmail.com>
2023-08-01 13:11:53 -0400

    Big checkpoint

--- README.md
+++ README.md
@@ -14,18 +14,23 @@ I am not responsible for any bans or data loss as a result of using this. This w
 * <b>Note:</b> The game must be in fullscreen at 1080p. Other methods might eventually be supported. You can always fix this yourself though.
 
 # Notes and tips
-I do not recommend relying on this to always keep you alive. It may catch or save you but it is not a quaranteed replacement for your fingers. I do not think it is fast enough for that yet. I am currently working on speeding this up. </br>
+I do not recommend relying on this to always keep you alive. It does a pretty good job but do not expect it to replace for your fingers. I do not think it is fast enough for that. </br>
 
-Currently on my system, it runs at around 40-50 Hz. Your system may vary. Once again, I am currently working on speeding this up. 60 Hz minimum is the goal. </br>
+On my system, it runs at around 55-65 Hz. Your system may vary. I am trying to make this as fast as possible. </br>
 
-I recommend to run PoE at 60 FPS. Running it too high will cause the game to get choppy and it might slow down the script. </br>
+I recommend to run PoE at 60 FPS. Running it too high will cause the game to get choppy and it might slow down the script. From personal experience, running at 144 FPS is way more choppy than at 60 FPS. </br>
+
+Currently, pixel values only detect if something is empty. This works easily but it not the most customizable. This may be improved in the future. </br>
 
 # Usage
-Clone the repo, edit the `config.yaml`, run `__main__.py`. </br>
+Clone the repo, edit the `config.yaml`, run `python3 sudo __main__.py -f <path to config file>`. </br>
 
-You can run the `kp_calc.py` script in utils to get a calculated data set for your key presses to use in the config. You can use any numbers you want, I just recommend using actual data as it represents the physical capabilities of your keyboard and should theoretically make your key presses look more human. If your keyboard key presses physically cannot be shorter than 28 ms, you shouldn't be emulating a 2 ms key press. </br>
+`-f | --file:` The location of your config.yaml (case sensitive). You can use relative paths like ~ . Will default to the included one if not set. Ex: `~/.config/poepyautopot/config.yaml`
+
+You can run the `kp_calc.py` script in utils to get a calculated data set for your key presses to use in the config. Just run it and follow the prompt. You can use any numbers you want, I just recommend using actual data as it represents the physical capabilities of your keyboard and should theoretically make your key presses look more human. If your keyboard key presses physically cannot be shorter than 28 ms, you shouldn't be emulating a 2 ms key press. </br>
 
 # Configuration (config.yaml)
+If it doesn't seem to work properly, please make sure your configurations are correctly set. As a last resort, open an issue on the GitHub.
 ## Flask database
 This is still a WIP. </br>
 Duration is still an experimental variable. </br>
@@ -35,7 +40,7 @@ It is not recommended to change anything unless you know what you are doing. </b
 ## User config
 #### Hardware
 `keyboard_event:` The `/dev/input/event#` that applies to your keyboard. </br>
-`screen_offset:` The space of screen above and to the left of the game window. This assumes the game is fullscreen at 1080p! </br>
+`screen_offset:` The space of screen above and to the left of the game window. This assumes the game is fullscreen at 1080p! Ex: If you have two horzontal 1080p monitors, and you run the game on the right one, set the X to 1920 and Y to 0. If you have 2 vertical 1080p monitors and you run the game on the bottom one, set the X to 0 and Y to 1080. </br>
 
 #### WRITE THESE IN SECONDS! They will get converted to milliseconds. </br>
 These will use my data by default. </br>
@@ -46,22 +51,28 @@ These will use my data by default. </br>
 `key_press: target:` The target mean length of your key press. Most of the lengths should be around this number. <br>
 
 #### Flasks
-`flask#: enable:` Booleam to enable the flask to be automated. </br>
-`flask#: type:` What kind of flask it is. Ex: A sacred life flask. To set this, you must use the yaml anchor in the database. </br>
-`flask#: react:` This will cause the flask to react to either life or mana. This will prtimarily be used for utility/unique flasks. </br>
-`flask#: x_offset:` The pixel offset of the capture. Don't change this unless you know what you are doing. </br>
+`flask#: enable:` Boolean to enable the flask to be automated. </br>
+`flask#: type:` What kind of flask it is. Ex: A sacred life flask. To set this, you must use the yaml anchor from the database. </br>
+`flask#: react:` This will cause the flask to react to either life or mana. This can be used to trigger utility/unique flasks. </br>
+`flask#: always:` This will cause the flask to trigger every single time life/mana is needed. Flasks with this enabled should be put towards the right of your flask menu. Useful with utility/unique flasks. </br>
 
 #### Functions
 `main: enable:` Boolean to enable the main part of the script. You should probably keep this enabled. </br>
 `main: life:` Boolean to enable the life checking. Disable if you don't want your life flasks automated. </br>
 `main: mana:` Same thing as `main: life:`, just for mana. </br>
-`main: flasks:` Boolean to enable flask automation. You should probably also keep this enabled. </br>
+`main: flasks:` Boolean to enable flask automation. You should probably also keep this enabled. Why else would you be using this script? </br>
 
 `debug: enable:` Boolean to enable debugging. This just shows some extra details while running. May impact performance. Recommended to keep this off. </br>
+`debug: life: enable:` Boolean to enable life specific debugging. </br>
+`debug: life: pixel:` Boolean to enable pixel detection debugging. Shows what pixel it expected to detect and what it actually detected. </br>
+`debug: life: rgb:` Boolean to enable RGB debugging. Shows what RGB value it expected to detect and what it actually detected. </br>
+`debug: mana:` Exact same as life. </br>
+`debug: flask:` Exact same as life. </br>
+* Note: The flasks have x offsets! What it actually detects is offset from what it expected based on the slot its in. Don't get confused by this. </br>
 `debug: image_save: enable:` Boolean to enable the saving of images while running. This will MASSIVELY tank performance so its HIGHLY recommend to keep this off. </br>
 `debug: image_save: location:` The place you want to save the images to. </br>
 
-`test: enable:` Boolean to enable testing functions. </br>
+`test: enable:` Boolean to enable testing functions. You should disable `main: enable:` when using this. </br>
 `test: key_press_test: enable:` Boolean to enable the key_press test. </br>
 `test: key_press_test: delay:` Time before the test actually begins. Gives you time to move your cursor to where you want it. </br>
 `test: key_press_test: count:` The amount of key_presses to simulate. </br>
--- poepyautopot/__main__.py
+++ poepyautopot/__main__.py
@@ -18,15 +18,31 @@ import yaml
 def config_init():
   # Turn variables from config.yaml into usable variables in the script.
   parser = argparse.ArgumentParser()
-  parser.add_argument("-f", "--file", required=True)
+  parser.add_argument("-f", "--file", default="./config.yaml")
   config_file = parser.parse_args().file
 
   yaml_config = yaml.safe_load(open(config_file))
 
-  global keyboard_event, screen_offset_x, screen_offset_y
-  keyboard_event = yaml_config["keyboard_event"] #g
+  #g just means the variable is globally assigned. Makes reading easier.
+  global life_pixel, life_empty, mana_pixel, mana_empty, flask1_offset_x, flask2_offset_x, flask3_offset_x, flask4_offset_x, flask5_offset_x
+  database = yaml_config["database"]
+  life = database["life"]
+  life_pixel = life["pixel"] #g
+  life_empty = life["empty"] #g
+  mana = database["mana"]
+  mana_pixel = mana["pixel"] #g
+  mana_empty = mana["empty"] #g
+  database_flask = database["flask"]
+  flask1_offset_x = database_flask["flask1_offset_x"] #g
+  flask2_offset_x = database_flask["flask2_offset_x"] #g
+  flask3_offset_x = database_flask["flask3_offset_x"] #g
+  flask4_offset_x = database_flask["flask4_offset_x"] #g
+  flask5_offset_x = database_flask["flask5_offset_x"] #g
 
-  screen_offset = yaml_config["screen_offset"]
+  global keyboard_event, screen_offset_x, screen_offset_y
+  hardware = yaml_config["hardware"]
+  keyboard_event = hardware["keyboard_event"] #g
+  screen_offset = hardware["screen_offset"]
   screen_offset_x = screen_offset["x"] #g
   screen_offset_y = screen_offset["y"] #g
 
@@ -38,7 +54,7 @@ def config_init():
   key_press_std_dev = key_press["std_dev"] #g
   key_press_target = key_press["target"] #g
 
-  global flask1_enable, flask1_pixel, flask1_empty, flask1_duration, flask1_react, flask1_x_offset
+  global flask1_enable, flask1_pixel, flask1_empty, flask1_duration, flask1_react, flask1_always
   flask1 = yaml_config["flask1"]
   flask1_enable = flask1["enable"] #g
   flask1_type = flask1["type"]
@@ -46,9 +62,9 @@ def config_init():
   flask1_empty = flask1_type["empty"] #g
   flask1_duration = flask1_type["duration"] #g
   flask1_react = flask1["react"] #g
-  flask1_x_offset = flask1["x_offset"] #g
+  flask1_always = flask1["always"] #g
 
-  global flask2_enable, flask2_pixel, flask2_empty, flask2_duration, flask2_react, flask2_x_offset
+  global flask2_enable, flask2_pixel, flask2_empty, flask2_duration, flask2_react, flask2_always
   flask2 = yaml_config["flask2"]
   flask2_enable = flask2["enable"] #g
   flask2_type = flask2["type"]
@@ -56,9 +72,9 @@ def config_init():
   flask2_empty = flask2_type["empty"] #g
   flask2_duration = flask2_type["duration"] #g
   flask2_react = flask1["react"] #g
-  flask2_x_offset = flask2["x_offset"] #g
+  flask2_always = flask1["always"] #g
 
-  global flask3_enable, flask3_pixel, flask3_empty, flask3_duration, flask3_react, flask3_x_offset
+  global flask3_enable, flask3_pixel, flask3_empty, flask3_duration, flask3_react, flask3_always
   flask3 = yaml_config["flask3"]
   flask3_enable = flask3["enable"] #g
   flask3_type = flask3["type"]
@@ -66,9 +82,9 @@ def config_init():
   flask3_empty = flask3_type["empty"] #g
   flask3_duration = flask3_type["duration"] #g
   flask3_react = flask1["react"] #g
-  flask3_x_offset = flask3["x_offset"] #g
+  flask3_always = flask1["always"] #g
 
-  global flask4_enable, flask4_pixel, flask4_empty, flask4_duration, flask4_react, flask4_x_offset
+  global flask4_enable, flask4_pixel, flask4_empty, flask4_duration, flask4_react, flask4_always
   flask4 = yaml_config["flask4"]
   flask4_enable = flask4["enable"] #g
   flask4_type = flask4["type"]
@@ -76,9 +92,9 @@ def config_init():
   flask4_empty = flask4_type["empty"] #g
   flask4_duration = flask4_type["duration"] #g
   flask4_react = flask1["react"] #g
-  flask4_x_offset = flask4["x_offset"] #g
+  flask4_always = flask1["always"] #g
 
-  global flask5_enable, flask5_pixel, flask5_empty, flask5_duration, flask5_react, flask5_x_offset
+  global flask5_enable, flask5_pixel, flask5_empty, flask5_duration, flask5_react, flask5_always
   flask5 = yaml_config["flask5"]
   flask5_enable = flask5["enable"] #g
   flask5_type = flask5["type"]
@@ -86,21 +102,30 @@ def config_init():
   flask5_empty = flask5_type["empty"] #g
   flask5_duration = flask5_type["duration"] #g
   flask5_react = flask1["react"] #g
-  flask5_x_offset = flask5["x_offset"] #g
+  flask5_always = flask1["always"] #g
 
-  global main_enable, life_enable, mana_enable, flask_enable
+  global main_enable, main_life_enable, main_mana_enable, main_flask_enable
   main = yaml_config["main"]
   main_enable = main["enable"] #g
-  life_enable = main["life"] #g
-  mana_enable = main["mana"] #g
-  flask_enable = main["flask"] #g
+  main_life_enable = main["life"] #g
+  main_mana_enable = main["mana"] #g
+  main_flask_enable = main["flask"] #g
 
-  global debug_enable, image_save_enable, image_save_location
+  global debug_enable, debug_life_enable, debug_life_pixel, debug_life_rgb, debug_mana_enable, debug_mana_pixel, debug_mana_rgb, debug_flask_enable, debug_image_save_enable, debug_image_save_location
   debug = yaml_config["debug"]
   debug_enable = debug["enable"] #g
-  image_save = debug["image_save"]
-  image_save_enable = image_save["enable"] #g
-  image_save_location = image_save["location"] #g
+  debug_life = debug["life"]
+  debug_life_enable = debug_life["enable"] #g
+  debug_life_pixel = debug_life["pixel"] #g
+  debug_life_rgb = debug_life["rgb"] #g
+  debug_mana = debug["mana"]
+  debug_mana_enable = debug_mana["enable"] #g
+  debug_mana_pixel = debug_mana["pixel"] #g
+  debug_mana_rgb = debug_mana["rgb"] #g
+  debug_flask_enable = debug["flask"]
+  debug_image_save = debug["image_save"]
+  debug_image_save_enable = debug_image_save["enable"] #g
+  debug_image_save_location = debug_image_save["location"] #g
 
   global test_enable, key_press_test_enable, key_press_test_delay, key_press_test_count
   test = yaml_config["test"]
@@ -110,7 +135,14 @@ def config_init():
   key_press_test_delay = key_press_test["delay"] #g
   key_press_test_count = key_press_test["count"] #g
 
-config_init()
+  global life_need, mana_need, flask1_valid, flask2_valid, flask3_valid, flask4_valid, flask5_valid
+  life_need = False
+  mana_need = False
+  flask1_valid = True
+  flask2_valid = True
+  flask3_valid = True
+  flask4_valid = True
+  flask5_valid = True
 
 def key_press_init():
   global key_press_shortest_low, key_press_shortest_high, key_press_longest_low, key_press_longest_high
@@ -119,9 +151,12 @@ def key_press_init():
   key_press_longest_low = key_press_longest - key_press_range
   key_press_longest_high = key_press_longest + key_press_range
 
+config_init()
+key_press_init()
+
 
 # Utility
-async def key_press(num, wait):
+def key_press(num, wait):
   # Bell curve to more closely group presses
   def bell_curve(min, max, sig, mu):
     while True:
@@ -151,160 +186,173 @@ def screen_capture():
   screen_capture = ImageGrab.grab(bbox=(screen_offset_x, screen_offset_y, 1920 + screen_offset_x, 1080 + screen_offset_y))
   screen_load = screen_capture.load()
 
-  if debug_enable is True and image_save_enable is True:
+  if debug_enable == True and debug_image_save_enable == True:
     screen_capture.save("./screen.png")
 
-def life_check(life_need):
-  r, g, b = screen_load[101, 1005]
-  if 101 <= r <= 111 and 9 <= g <= 19 and 15 <= b <= 25:
-    life_need.value = False
+def life_check():
+  global life_need
+  xl_raw, yl_raw = ast.literal_eval(life_pixel)
+  rl, gl, bl = screen_load[xl_raw, yl_raw]
+  rl_empty, gl_empty, bl_empty, = ast.literal_eval(life_empty)
+  rl_empty_min, rl_empty_max = rl_empty - 5, rl_empty + 5
+  gl_empty_min, gl_empty_max = gl_empty - 5, gl_empty + 5
+  bl_empty_min, bl_empty_max = bl_empty - 5, bl_empty + 5
+  if rl_empty_min <= rl <= rl_empty_max and gl_empty_min <= gl <= gl_empty_max and bl_empty_min <= bl <= bl_empty_max:
+    life_need = False
   else:
-    life_need.value = True
+    life_need = True
 
-def mana_check(mana_need):
-  r, g, b = screen_load[1801, 1005]
-  if 8 <= r <= 18 and 71 <= g <= 81 and 150 <= b <= 160:
-    mana_need.value = False
+  if debug_enable == True and debug_life_enable == True:
+    print(f"life-pixel[exp{main_life_pixel} - act{xl_raw, yl_raw}]")
+    print(f"life-rgb[exp({main_life_empty}) - act{rl, gl, bl}]")
+
+def mana_check():
+  global mana_need
+  xm_raw, ym_raw = ast.literal_eval(mana_pixel)
+  rm, gm, bm = screen_load[xm_raw, ym_raw]
+  rm_empty, gm_empty, bm_empty, = ast.literal_eval(mana_empty)
+  rm_empty_min, rm_empty_max = rm_empty - 5, rm_empty + 5
+  gm_empty_min, gm_empty_max = gm_empty - 5, gm_empty + 5
+  bm_empty_min, bm_empty_max = bm_empty - 5, bm_empty + 5
+  if rm_empty_min <= rm <= rm_empty_max and gm_empty_min <= gm <= gm_empty_max and bm_empty_min <= bm <= bm_empty_max:
+    mana_need = False
   else:
-    mana_need.value = True
+    mana_need = True
 
-def flask_check(flask1_valid, flask2_valid, flask3_valid, flask4_valid, flask5_valid):
-  if flask1_enable is True:
+  if debug_enable == True and debug_mana_enable == True:
+    print(f"mana-pixel[exp{main_mana_pixel} - act{xm_raw, ym_raw}]")
+    print(f"mana-rgb[exp({main_mana_empty}) - act{rm, gm, bm}]")
+
+def flask_check():
+  global flask1_valid, flask2_valid, flask3_valid, flask4_valid, flask5_valid
+  if flask1_enable == True:
     x1_raw, y1_raw = ast.literal_eval(flask1_pixel)
-    x1_off = (int(x1_raw) + int(flask1_x_offset))
+    x1_off = (int(x1_raw) + int(flask1_offset_x))
     r1, g1, b1 = screen_load[x1_off, y1_raw]
     r1_empty, g1_empty, b1_empty, = ast.literal_eval(flask1_empty)
     r1_empty_min, r1_empty_max = r1_empty - 5, r1_empty + 5
     g1_empty_min, g1_empty_max = g1_empty - 5, g1_empty + 5
     b1_empty_min, b1_empty_max = b1_empty - 5, b1_empty + 5
     if r1_empty_min <= r1 <= r1_empty_max and g1_empty_min <= g1 <= g1_empty_max and b1_empty_min <= b1 <= b1_empty_max:
-      flask1_valid.value = False
+      flask1_valid = False
     else:
-      flask1_valid.value = True
+      flask1_valid = True
 
-  if flask2_enable is True:
+  if flask2_enable == True:
     x2_raw, y2_raw = ast.literal_eval(flask2_pixel)
-    x2_off = (int(x2_raw) + int(flask2_x_offset))
+    x2_off = (int(x2_raw) + int(flask2_offset_x))
     r2, g2, b2 = screen_load[x2_off, y2_raw]
     r2_empty, g2_empty, b2_empty, = ast.literal_eval(flask2_empty)
     r2_empty_min, r2_empty_max = r2_empty - 5, r2_empty + 5
     g2_empty_min, g2_empty_max = g2_empty - 5, g2_empty + 5
     b2_empty_min, b2_empty_max = b2_empty - 5, b2_empty + 5
     if r2_empty_min <= r2 <= r2_empty_max and g2_empty_min <= g2 <= g2_empty_max and b2_empty_min <= b2 <= b2_empty_max:
-      flask2_valid.value = False
+      flask2_valid = False
     else:
-      flask2_valid.value = True
+      flask2_valid = True
 
-  if flask3_enable is True:
+  if flask3_enable == True:
     x3_raw, y3_raw = ast.literal_eval(flask3_pixel)
-    x3_off = (int(x3_raw) + int(flask3_x_offset))
+    x3_off = (int(x3_raw) + int(flask3_offset_x))
     r3, g3, b3 = screen_load[x3_off, y3_raw]
     r3_empty, g3_empty, b3_empty, = ast.literal_eval(flask3_empty)
     r3_empty_min, r3_empty_max = r3_empty - 5, r3_empty + 5
     g3_empty_min, g3_empty_max = g3_empty - 5, g3_empty + 5
     b3_empty_min, b3_empty_max = b3_empty - 5, b3_empty + 5
     if r3_empty_min <= r3 <= r3_empty_max and g3_empty_min <= g3 <= g3_empty_max and b3_empty_min <= b3 <= b3_empty_max:
-      flask3_valid.value = False
+      flask3_valid = False
     else:
-      flask3_valid.value = True
+      flask3_valid = True
   
-  if flask4_enable is True:
+  if flask4_enable == True:
     x4_raw, y4_raw = ast.literal_eval(flask4_pixel)
-    x4_off = (int(x4_raw) + int(flask4_x_offset))
+    x4_off = (int(x4_raw) + int(flask4_offset_x))
     r4, g4, b4 = screen_load[x4_off, y4_raw]
     r4_empty, g4_empty, b4_empty, = ast.literal_eval(flask4_empty)
     r4_empty_min, r4_empty_max = r4_empty - 5, r4_empty + 5
     g4_empty_min, g4_empty_max = g4_empty - 5, g4_empty + 5
     b4_empty_min, b4_empty_max = b4_empty - 5, b4_empty + 5
     if r4_empty_min <= r4 <= r4_empty_max and g4_empty_min <= g4 <= g4_empty_max and b4_empty_min <= b4 <= b4_empty_max:
-      flask4_valid.value = False
+      flask4_valid = False
     else:
-      flask4_valid.value = True
+      flask4_valid = True
 
-  if flask5_enable is True:
+  if flask5_enable == True:
     x5_raw, y5_raw = ast.literal_eval(flask5_pixel)
-    x5_off = (int(x5_raw) + int(flask5_x_offset))
+    x5_off = (int(x5_raw) + int(flask5_offset_x))
     r5, g5, b5 = screen_load[x5_off, y5_raw]
     r5_empty, g5_empty, b5_empty, = ast.literal_eval(flask5_empty)
     r5_empty_min, r5_empty_max = r5_empty - 5, r5_empty + 5
     g5_empty_min, g5_empty_max = g5_empty - 5, g5_empty + 5
     b5_empty_min, b5_empty_max = b5_empty - 5, b5_empty + 5
     if r5_empty_min <= r5 <= r5_empty_max and g5_empty_min <= g5 <= g5_empty_max and b5_empty_min <= b5 <= b5_empty_max:
-      flask5_valid.value = False
+      flask5_valid = False
     else:
-      flask5_valid.value = True
+      flask5_valid = True
+
+  if debug_enable == True and debug_flask_enable == True:
+    if flask1_enable == True:
+      print(f"flask1-pixel[exp{flask1_pixel} - act{x1_off, y1_raw}]")
+      print(f"flask1-rgb[exp({flask1_empty}) - act{r1, g1, b1}]")
+    if flask2_enable == True:
+      print(f"flask2-pixel[exp{flask2_pixel} - act{x2_off, y2_raw}]")
+      print(f"flask2-rgb[exp({flask2_empty}) - act{r2, g2, b2}]")
+    if flask3_enable == True:
+      print(f"flask3-pixel[exp{flask3_pixel} - act{x3_off, y3_raw}]")
+      print(f"flask3-rgb[exp({flask3_empty}) - act{r3, g3, b3}]")
+    if flask4_enable == True:
+      print(f"flask4-pixel[exp{flask3_pixel} - act{x4_off, y4_raw}]")
+      print(f"flask4-rgb[exp({flask3_empty}) - act{r4, g4, b4}]")
+    if flask5_enable == True:
+      print(f"flask5-pixel[exp{flask3_pixel} - act{x5_off, y5_raw}]")
+      print(f"flask5-rgb[exp({flask3_empty}) - act{r5, g5, b5}]")
 
 # Main
-async def main():
-  key_press_init()
-
-  manager = multiprocessing.Manager()
-  life_need = manager.Value('b', False)
-  mana_need = manager.Value('b', False)
-  flask1_valid = manager.Value('b', True)
-  flask2_valid = manager.Value('b', True)
-  flask3_valid = manager.Value('b', True)
-  flask4_valid = manager.Value('b', True)
-  flask5_valid = manager.Value('b', True)
-  
+def main():
   i = 0
   while True:
     screen_capture()
+    if main_life_enable == True:
+      life_check()
+    if main_mana_enable == True:
+      mana_check()
+    if main_flask_enable == True:
+      flask_check()
 
-    check_processes = []
-    if life_enable is True:
-      life_check_process = multiprocessing.Process(target=life_check, args=[life_need])
-      check_processes.append(life_check_process)
-    if mana_enable is True:
-      mana_check_process = multiprocessing.Process(target=mana_check, args=[mana_need])
-      check_processes.append(mana_check_process)
-    if flask_enable is True:
-      flask_check_process = multiprocessing.Process(target=flask_check, args=[flask1_valid, flask2_valid, flask3_valid, flask4_valid, flask5_valid])
-      check_processes.append(flask_check_process)
+    print(f"{i} life-{life_need} mana-{mana_need} 1-{flask1_valid} 2-{flask2_valid} 3-{flask3_valid} 4-{flask4_valid} 5-{flask5_valid}")
 
-    for process in check_processes:
-      process.start()
-    for process in check_processes:
-      process.join()
-
-    print(f"{i} life-{life_need.value} mana-{mana_need.value} 1-{flask1_valid.value} 2-{flask2_valid.value} 3-{flask3_valid.value} 4-{flask4_valid.value} 5-{flask5_valid.value}")
-
-    key_press_threads = []
-    if life_need.value is True:
-      if flask1_valid.value is True and flask1_enable is True and flask1_react == "Life":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_1, flask1_duration)))
-      elif flask2_valid.value is True and flask2_enable is True and flask2_react == "Life":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_2, flask2_duration)))
-      elif flask3_valid.value is True and flask3_enable is True and flask3_react == "Life":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_3, flask3_duration)))
-      elif flask4_valid.value is True and flask4_enable is True and flask4_react == "Life":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_4, flask4_duration)))
-      elif flask5_valid.value is True and flask5_enable is True and flask5_react == "Life":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_5, flask5_duration)))
+    if life_need == True:
+      if (flask1_enable == True and flask1_valid == True) and (flask1_react == "Life" or flask1_always):
+        key_press(e.KEY_1, flask1_duration)
+      elif (flask2_enable == True and flask2_valid == True) and (flask2_react == "Life" or flask2_always):
+        key_press(e.KEY_2, flask2_duration)
+      elif (flask3_enable == True and flask3_valid == True) and (flask3_react == "Life" or flask3_always):
+        key_press(e.KEY_3, flask3_duration)
+      elif (flask4_enable == True and flask4_valid == True) and (flask4_react == "Life" or flask4_always):
+        key_press(e.KEY_4, flask4_duration)
+      elif (flask5_enable == True and flask5_valid == True) and (flask5_react == "Life" or flask5_always):
+        key_press(e.KEY_5, flask5_duration)
       else:
         print("Life tick not available")
 
-    if mana_need.value is True:
-      if flask1_valid.value is True and flask1_enable is True and flask1_react == "Mana":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_1, flask1_duration)))
-      elif flask2_valid.value is True and flask2_enable is True and flask2_react == "Mana":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_2, flask2_duration)))
-      elif flask3_valid.value is True and flask3_enable is True and flask3_react == "Mana":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_3, flask3_duration)))
-      elif flask4_valid.value is True and flask4_enable is True and flask4_react == "Mana":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_4, flask4_duration)))
-      elif flask5_valid.value is True and flask5_enable is True and flask5_react == "Mana":
-        key_press_threads.append(asyncio.ensure_future(key_press(e.KEY_5, flask5_duration)))
+    if mana_need == True:
+      if (flask1_valid == True and flask1_enable == True) and (flask1_react == "Mana" or flask1_always):
+        key_press(e.KEY_1, flask1_duration)
+      elif (flask2_valid == True and flask2_enable == True) and (flask2_react == "Mana" or flask2_always):
+        key_press(e.KEY_2, flask2_duration)
+      elif (flask3_valid == True and flask3_enable == True) and (flask3_react == "Mana" or flask3_always):
+        key_press(e.KEY_3, flask3_duration)
+      elif (flask4_valid == True and flask4_enable == True) and (flask4_react == "Mana" or flask4_always):
+        key_press(e.KEY_4, flask4_duration)
+      elif (flask5_valid == True and flask5_enable == True) and (flask5_react == "Mana" or flask5_always):
+        key_press(e.KEY_5, flask5_duration)
       else:
         print("Mana tick not available")
-    
-    await asyncio.gather(*key_press_threads)
 
     i += 1
 
-if main_enable is True:
-  asyncio.run(main())
-  #screen_capture()
+if main_enable == True:
+  main()
 
 # Tests
 def key_press_test():
@@ -323,8 +371,8 @@ def key_press_test():
     i -= 1
 
 def test():
-  if key_press_test_enable is True:
+  if key_press_test_enable == True:
     key_press_test()
 
-if test_enable is True:
+if test_enable == True:
   test()
